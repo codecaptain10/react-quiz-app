7a7ed455b6dd8ede932f2c4484808bc5871b9158
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-05 14:54:08 +0100

    Download image for background, font style and example question from API

--- react-quiz-app/src/App.tsx
+++ react-quiz-app/src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 function App() {
+  //Link to Question from API: https://opentdb.com/api.php?amount=10&category=18&type=multiple
+  //Use catamaran fonr: font-family: 'Catamaran', sans-serif;
   return <div className='App'>Hello QuizApp</div>;
 }
 
