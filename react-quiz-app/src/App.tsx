96332b32c5f4603dc976a0c0ceaa0482f8c42dbd
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-06 22:58:30 +0100

    Create two files to data and create basic structure of App component

--- react-quiz-app/src/App.tsx
+++ react-quiz-app/src/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+//Coponents
+import QuestionCard from "./components/QuastionCard";
 
 function App() {
   //Link to Question from API: https://opentdb.com/api.php?amount=10&category=18&type=multiple
   //Use catamaran fonr: font-family: 'Catamaran', sans-serif;
-  return <div className='App'>Hello QuizApp</div>;
+  const startQuiz = async () => {};
+
+  const checkAnswer = (event: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div className='App'>
+      <h1>React Quiz App</h1>
+      <button className='start' onClick={startQuiz}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Questions ...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}>
+        Next Question
+      </button>
+    </div>
+  );
 }
 
 export default App;
--- react-quiz-app/src/components/QuastionCard.tsx
+++ react-quiz-app/src/components/QuastionCard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const QuestionCard = () => {
-  <div>Question Card</div>;
-};
+function QuestionCard() {
+  return <div>Question Card</div>;
+}
 
 export default QuestionCard;
