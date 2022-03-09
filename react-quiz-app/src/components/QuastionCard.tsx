4b1fecee1ad904bb3527eb8a8ded50c0178c66d2
Szymon Szaminko <sz.szaminko@gmail.com>
2022-03-09 19:42:39 +0100

    Create QuestionCard style file and finish the project

--- react-quiz-app/src/components/QuastionCard.tsx
+++ react-quiz-app/src/components/QuastionCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 //Types
 import { AnswerObject } from "../App";
+//Styles
+import { Wrapper, ButtonWrapper } from "./QuestionCard.styles";
 
 type Props = {
   question: string;
@@ -20,24 +22,29 @@ const QuestionCard: React.FC<Props> = ({
   totalQuestions,
 }) => {
   return (
-    <div>
-      <p className='number'>
-        Question: {questionNr} / {totalQuestions}
-      </p>
-      <p dangerouslySetInnerHTML={{ __html: question }} />
+    <Wrapper>
       <div>
-        {answers.map(answer => (
-          <div key={answer}>
-            <button
-              disabled={userAnswer ? true : false}
-              value={answer}
-              onClick={callback}>
-              <span dangerouslySetInnerHTML={{ __html: answer }} /> ...
-            </button>
-          </div>
-        ))}
+        <p className='number'>
+          Question: {questionNr} / {totalQuestions}
+        </p>
+        <p dangerouslySetInnerHTML={{ __html: question }} />
+        <div>
+          {answers.map(answer => (
+            <ButtonWrapper
+              key={answer}
+              correct={userAnswer?.correctAnswer === answer}
+              userClicked={userAnswer?.answer === answer}>
+              <button
+                disabled={userAnswer ? true : false}
+                value={answer}
+                onClick={callback}>
+                <span dangerouslySetInnerHTML={{ __html: answer }} /> ...
+              </button>
+            </ButtonWrapper>
+          ))}
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 
